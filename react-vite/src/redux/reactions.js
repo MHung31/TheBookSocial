68efdd3e868978d60c461da81de9ca543b8c9c37
Matthew <matthewhung.shop@gmail.com>
2024-01-25 18:51:55 -0500

    delete reaction completed

--- react-vite/src/redux/reactions.js
+++ react-vite/src/redux/reactions.js
@@ -3,6 +3,24 @@ const ADD_REACTION = "reactions/add";
 const DELETE_REACTION = "reactions/delete";
 const RESET_REACTIONS = "reactions/reset";
 
+const deleteReaction = (reactionId) => ({
+  type: DELETE_REACTION,
+  payload: reactionId,
+});
+
+export const thunkDeleteReaction = (reactionId) => async (dispatch) => {
+  const response = await fetch(`/api/reactions/${reactionId}`, {
+    method: "DELETE",
+  });
+  if (response.ok) {
+    const data = await response.json();
+    if (data.errors) {
+      return;
+    }
+    dispatch(deleteReaction(reactionId));
+  }
+};
+
 const resetReactions = () => ({
   type: RESET_REACTIONS,
   payload: null,
@@ -67,7 +85,10 @@ function reactionsReducer(reactionStore = initialState, action) {
       new_reactions = { ...reactionStore };
       new_reactions[action.payload.id] = action.payload;
       return new_reactions;
-
+    case DELETE_REACTION:
+      new_reactions = { ...reactionStore };
+      delete new_reactions[action.payload];
+      return new_reactions;
     default:
       return reactionStore;
   }
