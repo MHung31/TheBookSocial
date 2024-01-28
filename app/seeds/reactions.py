63777c4ef7e95aac2fafdef8f5b78598f156e362
Matthew <matthewhung.shop@gmail.com>
2024-01-28 15:52:56 -0500

    updating

---
+++ app/seeds/reactions.py
@@ -0,0 +1,13 @@
+from app.models import db, Reaction,  environment, SCHEMA
+from sqlalchemy.sql import text
+
+def seed_reactions():
+    return
+
+def undo_reactions():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.reactions RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM reactions"))
+
+    db.session.commit()
